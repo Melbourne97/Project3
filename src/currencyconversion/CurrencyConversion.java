1fb4d556573fb9302e2208932e871882c33f9a7e
MUP-Computer Science Club <csclub@mansfield.edu>
2018-11-09 12:39:01 -0500

    Added Functionality

--- src/currencyconversion/CurrencyConversion.java
+++ src/currencyconversion/CurrencyConversion.java
@@ -3,22 +3,21 @@ package currencyconversion;
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.geometry.*;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.RadioButton;
 import javafx.scene.control.TextField;
 import javafx.scene.control.ToggleGroup;
-import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.HBox;
-import javafx.scene.layout.VBox;
+import javafx.scene.layout.*;
 import javafx.stage.Stage;
 
 public class CurrencyConversion extends Application {
     
     private Scene scene;
     private Button calculate;
-    private RadioButton FutureValueChoice;
+    private RadioButton futureValueChoice;
     private String principalString;
     private TextField principalValue;
     private String rateString;
@@ -30,28 +29,24 @@ public class CurrencyConversion extends Application {
     private String amountString;
     private TextField amountValue;
     
-    private final int sceneWidth = 500;
-    private final int sceneHeight = 400;
+    private final int SCENE_WIDTH = 400;
+    private final int SCENE_HEIGHT = 250;
+    private final Insets INSET = new Insets(5, 5, 5, 5);
     
     
     @Override
-    public void start(Stage primaryStage) {
-        final double dollarsPerPound = 1.31;
-        final double poundsPerDollars = .76;
-        
+    public void start(Stage primaryStage) {        
         drawVisualInterface();
         
         calculate.setOnAction(new EventHandler<ActionEvent>(){
-            
             @Override
             public void handle(ActionEvent event){
-                convertCurrency(dollarsPerPound, poundsPerDollars);
+                Calculate();
             }
         });
         
         primaryStage.setTitle("Financial App");
         primaryStage.setScene(scene);
-        
         primaryStage.show();
     }
 
@@ -66,21 +61,21 @@ public class CurrencyConversion extends Application {
         borderPane.setCenter(setUpCurrencyInformation());
         calculate = new Button("Calculate");
         borderPane.setBottom(calculate);
-        scene = new Scene(borderPane, sceneWidth, sceneHeight);
+        scene = new Scene(borderPane, SCENE_WIDTH, SCENE_HEIGHT);
     }
     
     //choices between radio buttons future value and present value
     private HBox setUpCurrencyChoices(){
-        FutureValueChoice = new RadioButton("Future Value");
+        futureValueChoice = new RadioButton("Future Value");
         RadioButton presentChoice = new RadioButton("Present Value");
-        FutureValueChoice.setSelected(true);
+        futureValueChoice.setSelected(true);
         
         ToggleGroup tg = new ToggleGroup();
-        FutureValueChoice.setToggleGroup(tg);
+        futureValueChoice.setToggleGroup(tg);
         presentChoice.setToggleGroup(tg);
         
         HBox currencyChoices = new HBox();
-        currencyChoices.getChildren().add(FutureValueChoice);
+        currencyChoices.getChildren().add(futureValueChoice);
         currencyChoices.getChildren().add(presentChoice);
         
         return currencyChoices;
@@ -89,8 +84,12 @@ public class CurrencyConversion extends Application {
     //dont think its needed but keeping just incase it is
     private VBox setUpCurrencyInformation(){
         VBox currencyInformation = new VBox();
-        currencyInformation.getChildren().add(setUpPoundsInformation());
-        currencyInformation.getChildren().add(setUpDollarsInformation());
+        currencyInformation.getChildren().add(setUpPrincipalInformation());
+        currencyInformation.getChildren().add(setUpRateInformation());
+        currencyInformation.getChildren().add(setUpTimeInformation());
+        currencyInformation.getChildren().add(setUpNumberOfCompoundingsInformation());
+        currencyInformation.getChildren().add(setUpAmountInformation());
+        currencyInformation.setSpacing(1);
         
         return currencyInformation;
     }
@@ -98,7 +97,9 @@ public class CurrencyConversion extends Application {
     //Principal text area
     private HBox setUpPrincipalInformation(){
         Label principalName = new Label("Principal");
+        principalName.setPadding(INSET);
         principalValue = new TextField(principalString);
+        principalValue.setText("15000");
         
         HBox principalInformation = new HBox();
         principalInformation.getChildren().add(principalName);
@@ -110,7 +111,9 @@ public class CurrencyConversion extends Application {
     //Rate text area
     private HBox setUpRateInformation(){
         Label rateName = new Label("Rate");
+        rateName.setPadding(INSET);
         rateValue = new TextField(rateString);
+        rateValue.setText(".05");
         
         HBox rateInformation = new HBox();
         rateInformation.getChildren().add(rateName);
@@ -122,7 +125,9 @@ public class CurrencyConversion extends Application {
     //Time text area
     private HBox setUpTimeInformation(){
         Label timeName = new Label("Time");
+        timeName.setPadding(INSET);
         timeValue = new TextField(timeString);
+        timeValue.setText("30");
         
         HBox timeInformation = new HBox();
         timeInformation.getChildren().add(timeName);
@@ -134,7 +139,9 @@ public class CurrencyConversion extends Application {
     //Number of Compoundings text area
     private HBox setUpNumberOfCompoundingsInformation(){
         Label numberOfCompoundingsName = new Label("Number Of Compoundings");
+        numberOfCompoundingsName.setPadding(INSET);
         numberOfCompoundingsValue = new TextField(numberOfCompoundingsString);
+        numberOfCompoundingsValue.setText("12");
         
         HBox numberOfCompoundingsInformation = new HBox();
         numberOfCompoundingsInformation.getChildren().add(numberOfCompoundingsName);
@@ -146,6 +153,7 @@ public class CurrencyConversion extends Application {
     //Amount text area
     private HBox setUpAmountInformation(){
         Label amountName = new Label("Amount");
+        amountName.setPadding(INSET);
         amountValue = new TextField(amountString);
         
         HBox amountInformation = new HBox();
@@ -156,15 +164,21 @@ public class CurrencyConversion extends Application {
     }
     
     //needs calculations for future value and present value
-    private void Calculate(double dollarsPerPound, double poundsPerDollars){
-        if(FutureValueChoice.isSelected()){
-            double dollars = Double.parseDouble(dollarsValue.getText());
-            double pounds = poundsPerDollars * dollars;
-            poundsValue.setText(String.valueOf(pounds));
+    private void Calculate(){
+        if(futureValueChoice.isSelected()){
+            double principal = Double.parseDouble(principalValue.getText());
+            double rate = Double.parseDouble(rateValue.getText());
+            double time = Double.parseDouble(timeValue.getText());
+            double compoundings = Double.parseDouble(numberOfCompoundingsValue.getText());
+            double amount = principal * Math.pow(1 + rate / compoundings, compoundings * time);
+            amountValue.setText(String.valueOf(amount));
         }else{
-            double pounds = Double.parseDouble(poundsValue.getText());
-            double dollars = dollarsPerPound* pounds;
-            dollarsValue.setText(String.valueOf(dollars));
+            double rate = Double.parseDouble(rateValue.getText());
+            double time = Double.parseDouble(timeValue.getText());
+            double compoundings = Double.parseDouble(numberOfCompoundingsValue.getText());
+            double amount = Double.parseDouble(amountValue.getText());
+            double present = amount * Math.pow(1 + rate / compoundings,-compoundings * time);
+            principalValue.setText(String.valueOf(present));
         }
     }
     
